fb703dab546ca6a2b7ce343230a95ebd90dad92b
joe bebel <55120843+joebebel@users.noreply.github.com>
2021-12-10 23:05:28 -0800

    public

--- src/circuit/gadget/utilities/lookup_range_check.rs
+++ src/circuit/gadget/utilities/lookup_range_check.rs
@@ -114,7 +114,7 @@ impl<F: FieldExt + PrimeFieldBits, const K: usize> LookupRangeCheckConfig<F, K> 
         config
     }
 
-    #[cfg(test)]
+    //#[cfg(test)]
     // Loads the values [0..2^K) into `table_idx`. This is only used in testing
     // for now, since the Sinsemilla chip provides a pre-loaded table in the
     // Orchard context.
